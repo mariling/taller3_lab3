5e92b66c2f1cbc9a0055d17bc7ea6e7780854a85
Mariling <jimenezmrk34@gmail.com>
2026-03-20 11:33:38 -0500

    Cambios en el proyecto 1

---
+++ Grades.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Grades {
+  public static void main(String[] args) {
+
+    ArrayList<Integer> gradesList = new ArrayList<>();
+    Scanner reader = new Scanner(System.in);
+
+    int numberNotes = 5;
+    for (int i = 0; i < numberNotes; i++) {
+
+      System.out.println("Ingrese sus notas:" + (i + 1));
+      numberNotes = reader.nextLine();
+      int numberNotes = 
+
+    }
+
+  }
+}
+
+// Crear lista llmada grades
+// Guardar almenos 5 notas
+// Crear una funcion llamada calculateAverage
+// La funcion debe recibir una lista de notas
+// Debe devolver el promedio
+// Mostrar el resultado en pantalla
---
+++ ShoppingList.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ShoppingList {
+
+  public static void main(String[] args) {
+    
+    ArrayList<String>shoppingList =  new ArrayList<>();
+    Scanner reader = new (System.in);
+
+    //Agrego
+    shoppingList.add("Leche");
+    shoppingList.add("Huevos");
+    shoppingList.add("Pan");
+    shoppingList.add("Mantequilla");
+    shoppingList.add("Manzanas");
+
+    //Mostrar 
+      int = list
+    System.out.println("Lista de compras:");
+    for (int list = 0; list < shoppingList.size(); list++) 
+        System.out.println(shoppingList.get(list));
+
+    // Par agregar 
+    System.out.println("Ingrese el nuevo producto:");
+    String newProduct = scanner.nextLine();
+    shoppingList.add(newProduct);
+
+    // para borrar 
+    System.out.println("Ingrese el producto a Eliminar");
+    String 
+
+
+
+
+
+  }
+}
