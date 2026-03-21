a567b964ab0cc37b30663a76132a191d7b0f31a4
Mariling <jimenezmrk34@gmail.com>
2026-03-20 19:43:41 -0500

    Calculadora de notas terminado

--- Grades.java
+++ Grades.java
@@ -8,15 +8,26 @@ public class Grades {
     Scanner reader = new Scanner(System.in);
 
     int numberNotes = 5;
+    int userNotes = 0;
     for (int i = 0; i < numberNotes; i++) {
 
       System.out.println("Ingrese sus notas:" + (i + 1));
-      numberNotes = reader.nextLine();
-      int numberNotes = 
-
+      userNotes = Integer.parseInt(reader.nextLine()); // Para pasar de String a Int
+      gradesList.add(userNotes); // Guardo la nota ingresada
     }
-
+    reader.close();
+    System.out.println("Tu promedio es:" + calculateAverage(gradesList));
   }
+
+  public static int calculateAverage(ArrayList<Integer> gradesList) {
+    int additionNotes = 0;
+    for (Integer userNotes : gradesList) {
+      additionNotes = additionNotes + userNotes;
+    }
+    additionNotes = additionNotes / 5;
+    return additionNotes;
+  }
+
 }
 
 // Crear lista llmada grades
