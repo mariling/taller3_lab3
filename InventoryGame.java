b194831497565f024ac99e81c4b4ecb282719d48
Mariling <jimenezmrk34@gmail.com>
2026-03-26 14:43:11 -0500

    En curso

---
+++ InventoryGame.java
@@ -0,0 +1,11 @@
+public class InventoryGame {
+  public static void main(String[] args) {
+
+  }
+
+}
+
+// 1.Mostrar todos los objetos del inventario
+// 2. Eliminar un objeto
+// 3. Buscar si un objeto existe
+// 4. Crear una función llamada showInventory
