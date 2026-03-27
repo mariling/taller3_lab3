1431333549eac006be9134862b25d884cbe221bf
Mariling <jimenezmrk34@gmail.com>
2026-03-26 19:57:39 -0500

    uso de switch

--- InventoryGame.java
+++ InventoryGame.java
@@ -1,6 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class InventoryGame {
   public static void main(String[] args) {
 
+    ArrayList<String> inventoryGame = new ArrayList<>();
+    Scanner reader = new Scanner(System.in);
+
+    int option;
+
+    Collections.addAll(inventoryGame, "sword", "shield", "potion", "armor");
+    System.out.println(inventoryGame);
+
+    do {
+      System.out.println("ELIGE QUÉ HACER:");
+      System.out.println("1. Agregar objeto");
+      System.out.println("2. Mostrar inventario");
+      System.out.println("3. Eliminar objeto");
+      System.out.println("4. Buscar objeto");
+      System.out.println("5. Salir");
+
+      option.nextInt();
+
+    } while (condition);
+
+    read.close();
   }
 
 }
