1641455c4f8ff92b6313bfdc23ecec5d933ad8ee
Mariling <jimenezmrk34@gmail.com>
2026-03-27 19:24:01 -0500

    Desafio completo

--- TaskManager.java
+++ TaskManager.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class TaskManager {
   public static void main(String[] args) {
     ArrayList<String> checkList = new ArrayList<>();
+    ArrayList<String> checkTask = new ArrayList<>();
     Scanner reader = new Scanner(System.in);
 
-    Collections.addAll(checkList, "Estudiar francés", "Estudiar matemáticas", "Estudiar programación");
+    Collections.addAll(checkList, "frances", "matematicas", "programacion");
 
     System.out.println("TAREAS PENDIENTES:");
     for (int list = 0; list < checkList.size(); list++) {
@@ -22,14 +23,32 @@ public class TaskManager {
     String deleteTask = reader.nextLine();
     checkList.remove(deleteTask);
 
-    System.out.println("Tareas marcadas");
-    int checkTask = reader.nextInt();
-    checkList.set(checkTask, "[✔]" + checkList.get(checkTask));
+    // allTask.addAll(checkList);
 
-    System.out.println("TAREAS ACTUALIZADAS:");
-    for (int list = 0; list < checkList.size(); list++) {
-      System.out.println((list + 1) + "." + checkList.get(list));
-    }
+    System.out.println("Tareas marcadas");
+    String taskCheck = reader.nextLine();
+    checkList.remove(taskCheck);
+    checkTask.add(taskCheck);
+
+    System.out.println("Tareas pendientes: " + checkList);
+    System.out.println("Tareas actualizadas: " + checkTask);
+
+    // checkList.set(checkTask, "[✔]" + checkList.equals(checkTask));
+    /*
+     * System.out.println("checkTask" + checkTask);
+     * 
+     * for (int i = 0; i < checkList.size(); i++) {
+     * System.out.println((i + 1) + ". " + checkList.get(i));
+     * }
+     * 
+     * if (i= checkTask)
+     * checkList.setSelected(true);
+     */
+
+    // System.out.println("TAREAS ACTUALIZADAS:");
+    // for (int list = 0; list < checkList.size(); list++) {
+    // System.out.println((list + 1) + "." + checkList.get(list));
+    // }
     reader.close();
   }
 
